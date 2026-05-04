6b5f162a7b551537250eb8581b3094cc44134aa8
Tanu Verma <15.tanuverma@gmail.com>
2026-05-04 23:42:04 +0530

    this counts the number of words and alphabets in a file

---
+++ index.js
@@ -0,0 +1,25 @@
+const {program} = require('commander');
+const fs = require('fs');
+
+
+program
+    .name('reader')
+    .description('this counts to number of words and alphabets in a file');
+
+program.command('count')
+    .argument('<filepath>','file to count')
+    .action((filepath)=>{
+        fs.readFile(filepath,'utf8',(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                const lines = data.split(' ').length;
+                const alpha = data.split('').filter(a => a != ' ').length;
+                console.log(`number of words: ${lines}`);
+                console.log(`number of alphabets: ${alpha}`);
+            }
+        })
+    });
+ 
+    program.parse();
